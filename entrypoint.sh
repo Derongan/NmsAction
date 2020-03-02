157ddbdaf4993f15f918b88c75d0e86b1ccd614f
Derongan <dgfried@gmail.com>
2020-03-02 13:19:20 -0800

    Move back to home, it might be persisting?

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -l
+mkdir nms-build
+cd nms-build
 
 wget -O BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 
@@ -6,7 +8,5 @@ java -jar BuildTools.jar --rev $1
 
 # Install to github .m2
 mkdir -p $HOME/.m2/repository
-echo "$HOME contains"
-ls -lah $HOME
 echo "Copying from /root/.m2/repository to $HOME/.m2/repository"
-cp -a /root/.m2/repository/. ./.m2/repository
+cp -a /root/.m2/repository/. $HOME/.m2/repository
