fe55f26d6697c591311e06d6e20fd94eb744a8be
Derongan <dgfried@gmail.com>
2020-03-02 13:29:09 -0800

    Logging the contents of root m2

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,5 @@ java -jar BuildTools.jar --rev $1
 # Install to github .m2
 mkdir -p $HOME/.m2/repository
 echo "Copying from /root/.m2/repository to $HOME/.m2/repository"
+ls /root/.m2/repository
 cp -a /root/.m2/repository/. $HOME/.m2/repository
