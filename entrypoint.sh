8812bfa84915960bf972e1029f3c46e2ddbd6dbb
Derongan <dgfried@gmail.com>
2020-03-02 13:42:31 -0800

    Install to workspace instead of home

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,6 @@ wget -O BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSucce
 java -jar BuildTools.jar --rev $1
 
 # Install to github .m2
-mkdir -p $HOME/.m2/repository
-echo "Copying from /root/.m2/repository to $HOME/.m2/repository"
-ls /root/.m2/repository
-cp -a /root/.m2/repository/. $HOME/.m2/repository
+mkdir -p .m2/repository
+echo "Copying from /root/.m2/repository to $PWD/.m2/repository"
+cp -a /root/.m2/repository/. .m2/repository
